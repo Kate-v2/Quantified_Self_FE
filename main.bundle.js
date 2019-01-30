7ef4faac948833227bb142c8fa4db2373a00579c
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-29 18:03:00 -0700

    Confirm table building is dynamic

--- main.bundle.js
+++ main.bundle.js
@@ -59,7 +59,19 @@
 	// let headers = ["Food", "Calories"]
 	// mealTable.make_table_headers(table, headers)
 
-	var container = mealTable.make_breakfast_table();
+	var breakfast = mealTable.make_breakfast_table();
+	var lunch = mealTable.make_lunch_table();
+	var dinner = mealTable.make_dinner_table();
+	var snack = mealTable.make_snack_table();
+
+	var table = document.getElementById('breakfastTable');
+	// let data = { "Banana": 100 }
+	var data = {
+	  "Banana": 100,
+	  "Apple": 120,
+	  "Orange": 110
+	};
+	mealTable.make_table_row(table, data);
 
 /***/ }),
 /* 1 */
@@ -75,6 +87,10 @@
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+	// TO DO - move most of this to a FoodTable class,
+	// - Use that class to build these custom tables
+	// - Add Calorie statistics to those tables in this class
+
 	var MealTable = function () {
 	  function MealTable() {
 	    _classCallCheck(this, MealTable);
@@ -85,17 +101,38 @@
 	    value: function headers() {
 	      return ['Food', 'Calories'];
 	    }
+
+	    // ---- Methods for page use ----
+
 	  }, {
 	    key: 'make_breakfast_table',
 	    value: function make_breakfast_table() {
-	      // let section   = document.getElementById('tables')
-	      // let container = this.make_table_container(section, "breakfastContainer")
-	      // this.addContainerTitle(container, "Breakfast")
-	      // let table = this.make_table(container, 'breakfastTable')
-	      // this.make_table_headers(table, this.headers())
-	      // return container
-	      this.make_specific_table("Breakfast");
+	      var container = this.make_specific_table("Breakfast");
+	      var table = document.getElementById('breakfastTable');
+	      // this.make_table_row(table, {})
+	      return container;
 	    }
+	  }, {
+	    key: 'make_lunch_table',
+	    value: function make_lunch_table() {
+	      var container = this.make_specific_table("Lunch");
+	      return container;
+	    }
+	  }, {
+	    key: 'make_dinner_table',
+	    value: function make_dinner_table() {
+	      var container = this.make_specific_table("Dinner");
+	      return container;
+	    }
+	  }, {
+	    key: 'make_snack_table',
+	    value: function make_snack_table() {
+	      var container = this.make_specific_table("Snack");
+	      return container;
+	    }
+
+	    // ---- Table Container ------
+
 	  }, {
 	    key: 'make_specific_table',
 	    value: function make_specific_table(title) {
@@ -130,6 +167,9 @@
 	      p.className = 'mealTableTitle';
 	      container.appendChild(p);
 	    }
+
+	    // ---- Table Builder ----
+
 	  }, {
 	    key: 'make_table',
 	    value: function make_table(container) {
@@ -158,10 +198,29 @@
 	      }
 	    }
 	  }, {
+	    key: 'make_table_body',
+	    value: function make_table_body(table) {
+	      var body = table.createTBody();
+	      return body;
+	    }
+	  }, {
 	    key: 'make_table_row',
-	    value: function make_table_row(table) {
-	      var id = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
-	      var className = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'mealTableItem';
+	    value: function make_table_row(table, data) {
+	      var id = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+	      var className = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'mealTableItem';
+
+	      // TO DO - data will probably be an array of objects like:
+	      // { food: "Banana", calories: 100 }
+	      // If so, this method has to change!
+	      var body = table.children[1] || this.make_table_body(table);
+	      var i = 0;
+	      for (var key in data) {
+	        var row = body.insertRow(i);
+	        var cell1 = row.insertCell(0);
+	        var cell2 = row.insertCell(1);
+	        cell1.innerHTML = key;
+	        cell2.innerHTML = data[key];
+	      }
 	    }
 	  }]);
 
