83adf3345acc0f829579fb838e371a704c18becc
Katelyn <katelyn.rimkunas@gmail.com>
2019-01-31 17:57:07 -0700

    WIP - MealTable now has attributes

--- main.bundle.js
+++ main.bundle.js
@@ -138,68 +138,83 @@
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 	var MealTable = function () {
+	  function MealTable(id) {
+	    var className = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "meal";
 
-	  // TO DO - reduce this class by instantiating it as
-	  // a Breakfast / Lunch / Dinner / Snack object
-	  //  - make attributes for class, id & title
-	  //  - single method for make_meal_table
-	  //  - single method for make_stats
-
-	  // TO DO - reduce dependencies !!!
-
-	  // TO DO - give tbody for table data an id & class
-	  // --> easier to snag
-	  // --> reduce hardcoded array index
-	  function MealTable() {
 	    _classCallCheck(this, MealTable);
 
+	    this.idBase = id;
+	    this.className = className;
 	    this.stats = new _table_statistics2.default();
+	    this.foodTable = new _food_table2.default(this.className, this.title(this.idBase));
 	  }
 
-	  // ---- Methods for page use ----
-
 	  _createClass(MealTable, [{
-	    key: 'make_breakfast_table',
-	    value: function make_breakfast_table(base_element) {
-	      var foodTable = new _food_table2.default('meal', 'Breakfast');
-	      var container = foodTable.make_specific_table(base_element);
-	      return container;
+	    key: 'title',
+	    value: function title(word) {
+	      word.atChar(0).toUpperCase() + word.slice(1);
 	    }
+
+	    // TO DO - reduce this class by instantiating it as
+	    // a Breakfast / Lunch / Dinner / Snack object
+	    //  - make attributes for class, id & title
+	    //  - single method for make_meal_table
+	    //  - single method for make_stats
+
+	    // TO DO - reduce dependencies !!!
+
+	    // TO DO - give tbody for table data an id & class
+	    // --> easier to snag
+	    // --> reduce hardcoded array index
+
+
+	    // ---- Methods for page use ----
+
 	  }, {
-	    key: 'make_lunch_table',
-	    value: function make_lunch_table(base_element) {
-	      var foodTable = new _food_table2.default('meal', 'Lunch');
-	      var container = foodTable.make_specific_table(base_element);
+	    key: 'make_meal_table',
+	    value: function make_meal_table(base_element) {
+	      // let foodTable = new FoodTable(this.className, this.title(this.idBase))
+	      var container = this.foodTable.make_specific_table(base_element);
 	      return container;
 	    }
-	  }, {
-	    key: 'make_dinner_table',
-	    value: function make_dinner_table(base_element) {
-	      var foodTable = new _food_table2.default('meal', 'Dinner');
-	      var container = foodTable.make_specific_table(base_element);
-	      return container;
-	    }
-	  }, {
-	    key: 'make_snack_table',
-	    value: function make_snack_table(base_element) {
-	      var foodTable = new _food_table2.default('meal', 'Snack');
-	      var container = foodTable.make_specific_table(base_element);
-	      return container;
-	    }
+
+	    // make_breakfast_table(base_element) {
+	    //   let foodTable = new FoodTable(this.className, this.title(this.idBase))
+	    //   let container = foodTable.make_specific_table(base_element)
+	    //   return container
+	    // }
+	    //
+	    // make_lunch_table(base_element) {
+	    //   let foodTable = new FoodTable('meal', 'Lunch')
+	    //   let container = foodTable.make_specific_table(base_element)
+	    //   return container
+	    // }
+	    //
+	    // make_dinner_table(base_element) {
+	    //   let foodTable = new FoodTable('meal', 'Dinner')
+	    //   let container = foodTable.make_specific_table(base_element)
+	    //   return container
+	    // }
+	    //
+	    // make_snack_table(base_element) {
+	    //   let foodTable = new FoodTable('meal', 'Snack')
+	    //   let container = foodTable.make_specific_table(base_element)
+	    //   return container
+	    // }
 
 	    // ---- Table Builder ----
 
 	  }, {
 	    key: 'make_table_rows',
 	    value: function make_table_rows(table, data) {
-	      var foodTable = new _food_table2.default('meal', "Meal");
-	      foodTable.make_table_rows(table, data);
+	      // let foodTable = new FoodTable('meal', "Meal")
+	      this.foodTable.make_table_rows(table, data);
 	    }
 	  }, {
 	    key: 'make_table_row',
 	    value: function make_table_row(table, data) {
-	      var foodTable = new _food_table2.default('meal', "Meal");
-	      foodTable.make_table_row(table, data);
+	      // let foodTable = new FoodTable('meal', "Meal")
+	      this.foodTable.make_table_row(table, data);
 	    }
 	  }, {
 	    key: 'make_table_goal',
@@ -224,70 +239,85 @@
 	    // ---- Statistics ----
 
 	  }, {
-	    key: 'make_breakfast_stats',
-	    value: function make_breakfast_stats() {
-	      var stats = this.make_stats_section('meal', 'breakfast');
+	    key: 'make_meal_stats',
+	    value: function make_meal_stats() {
+	      // let stats = this.make_stats_section(this.className, this.idBase )
+	      var stats = this.make_stats_section();
 	      return stats;
 	    }
-	  }, {
-	    key: 'make_lunch_stats',
-	    value: function make_lunch_stats() {
-	      var stats = this.make_stats_section('meal', 'lunch');
-	      return stats;
-	    }
-	  }, {
-	    key: 'make_dinner_stats',
-	    value: function make_dinner_stats() {
-	      var stats = this.make_stats_section('meal', 'dinner');
-	      return stats;
-	    }
-	  }, {
-	    key: 'make_snack_stats',
-	    value: function make_snack_stats() {
-	      var stats = this.make_stats_section('meal', 'snack');
-	      return stats;
-	    }
+
+	    // make_breakfast_stats() {
+	    //   let stats = this.make_stats_section('meal', 'breakfast' )
+	    //   return stats
+	    // }
+	    //
+	    // make_lunch_stats() {
+	    //   let stats = this.make_stats_section('meal', 'lunch' )
+	    //   return stats
+	    // }
+	    //
+	    // make_dinner_stats() {
+	    //   let stats = this.make_stats_section('meal', 'dinner' )
+	    //   return stats
+	    // }
+	    //
+	    // make_snack_stats() {
+	    //   let stats = this.make_stats_section('meal', 'snack' )
+	    //   return stats
+	    // }
+
+
+	    // make_stats_section(className, id) {
+
 	  }, {
 	    key: 'make_stats_section',
-	    value: function make_stats_section(className, id) {
-	      var table = document.getElementById(id + 'Table');
+	    value: function make_stats_section() {
+	      var table = document.getElementById(this.idBase + 'Table');
 	      var tbody = table.children[1];
 	      var sum = this.stats.sum_table_body_rows(tbody, 1);
-	      var statsBody = this.stats.add_statistics_table_body(table, id, className);
-	      var sumRow = this.make_calorie_total(statsBody, sum, id, className);
-	      var resultRow = this.make_goal_total_row(sumRow, className, id);
+	      var statsBody = this.stats.add_statistics_table_body(table, this.idBase, this.className);
+	      // let sumRow    = this.make_calorie_total(statsBody, sum, this.idBase, this.className)
+	      var sumRow = this.make_calorie_total(statsBody, sum);
+	      // let resultRow = this.make_goal_total_row(sumRow, this.className, this.idBase)
+	      var resultRow = this.make_goal_total_row(sumRow);
 	      return statsBody;
 	    }
+
+	    // make_calorie_total(statBody, total, id=null, className=null) {
+
 	  }, {
 	    key: 'make_calorie_total',
 	    value: function make_calorie_total(statBody, total) {
-	      var id = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
-	      var className = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
-
 	      var data = { "Total Calories": total };
-	      var sum = this.stats.add_statistics_table_row(statBody, data, id, className);
+	      var sum = this.stats.add_statistics_table_row(statBody, data, this.idBase, this.className);
 	      return sum;
 	    }
+
+	    // make_goal_total_row(sumRow, className, id) {
+
 	  }, {
 	    key: 'make_goal_total_row',
-	    value: function make_goal_total_row(sumRow, className, id) {
+	    value: function make_goal_total_row(sumRow) {
 
 	      // TO DO - REFACTOR!
 	      // CELLS should have names for easy access
 
-	      var goal = this.stats.cell_value(document.getElementById(id + 'GoalCell'));
+	      var goal = this.stats.cell_value(document.getElementById(this.idBase + 'GoalCell'));
 	      var calories = this.stats.cell_value(sumRow.children[1]);
 	      var result = goal - calories || 0;
-	      var stats = document.getElementById(id + 'Statistics');
+	      var stats = document.getElementById(this.idBase + 'Statistics');
 	      var row = stats.insertRow();
 	      var cell1 = row.insertCell();
 	      cell1.innerHTML = "Remaining Calories";
 	      var cell2 = row.insertCell();
 	      cell2.innerHTML = result;
-	      cell2.id = id + 'CalorieResult';
-	      cell2.className = className + 'CalorieResult';
+	      cell2.id = this.idBase + 'CalorieResult';
+	      cell2.className = this.className + 'CalorieResult';
 	      if (result < 0) {
 	        cell2.classList.add('negative');
+	      }
+	      if (result > 0) {
+	        cell2.classList.add('positive');
 	      }
 	      return row;
 	    }
